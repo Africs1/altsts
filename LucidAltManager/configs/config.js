daa9da359e4f97ead4d4b61a97a1a0bab693af89
Africs1 <104873973+Africs1@users.noreply.github.com>
2022-06-05 23:06:23 +1000

    Update config.js

--- LucidAltManager/configs/config.js
+++ LucidAltManager/configs/config.js
@@ -1,17 +1,17 @@
 module.exports = {
-    botToken: "", // your discord bot token
-    license: "", // your license from Lucid Auth Bot
+    botToken: "OTI5NTQzMTIyNTI0OTk1NjI0.GqkDTL.gaulVJFaQBYmZFzL_f3NRo_nN-IJSMSGB8xCOI", // your discord bot token
+    license: "0.ab4rzjmdzo0.ory2m2gwaw0.q1hjvfyhtla0.zyod7yvwhm", // your license from Lucid Auth Bot
     adminRoleID: "", // the ID of your admin role
     ownerID: '', // Your ID
-    prefix: '', // your bot prefix
+    prefix: '.', // your bot prefix
     embedfooterText: "", // The text you want the footer to be
     embedColor: "", // the color of your embeds(hex codes)
-    serverIP: 't', // the IP of the server youre playing
+    serverIP: 'play.scorgemc.com', // the IP of the server youre playing
     port: "25565", // the port you want the alts to use(default: 25565)
     version: "1.8.9", // the version you want yours alts to login to
-    serverCommand: "", // your server join command(ex: /factions)
+    serverCommand: "/factions", // your server join command(ex: /factions)
     logChannelID: "", // your log channel ID
     preciseLogin: false, // use a special login sequence
     loginInterval: "10", // please state the time it takes to login alts in. Ex: 3 = 3 seconds
-    loginMethod: "microsoft" // the login method your alts will use(mojang or microsoft)
+    loginMethod: "mojang" // the login method your alts will use(mojang or microsoft)
 }
